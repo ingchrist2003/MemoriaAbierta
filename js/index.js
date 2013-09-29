66722634e8a37fbeb5978dd3a6a5be606161c0d1
ingchrist2003 <ingchrist2003@gmail.com>
2013-09-29 01:03:37 -0500

    testing

--- js/index.js
+++ js/index.js
@@ -261,5 +261,40 @@
 				  });
                 });
 				
-				
+function watchLocation(successCallback, errorCallback) {
+  successCallback = successCallback || function(){};
+  errorCallback = errorCallback || function(){};
+
+  // Try HTML5-spec geolocation.
+  var geolocation = navigator.geolocation;
+
+  if (geolocation) {
+    // We have a real geolocation service.
+    try {
+      function handleSuccess(position) {
+        successCallback(position.coords);
+      }
+
+      geolocation.watchPosition(handleSuccess, errorCallback, {
+        enableHighAccuracy: true,
+        maximumAge: 5000 // 5 sec.
+      });
+    } catch (err) {
+      errorCallback();
+    }
+  } else {
+    errorCallback();
+  }
+}
+
+function init() {
+  watchLocation(function(coords) {
+    document.getElementById('test').innerHTML = 'coords: ' + coords.latitude + ',' + coords.longitude;
+	latitud = coords.latitude;
+	longitud = coords.longitude;
+	creacionMapa();
+  }, function() {
+    document.getElementById('test').innerHTML = 'error';
+  });
+}
 				
