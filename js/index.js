cf92a4d1703e553343186a680004c14a9638b2f3
ingchrist2003 <ingchrist2003@gmail.com>
2013-09-29 01:16:01 -0500

    test

--- js/index.js
+++ js/index.js
@@ -261,41 +261,5 @@
 				  });
                 });
 				
-function watchLocation(successCallback, errorCallback) {
-  successCallback = successCallback || function(){};
-  errorCallback = errorCallback || function(){};
 
-  // Try HTML5-spec geolocation.
-  var geolocation = navigator.geolocation;
-
-  if (geolocation) {
-    // We have a real geolocation service.
-    try {
-      function handleSuccess(position) {
-        successCallback(position.coords);
-      }
-
-      geolocation.watchPosition(handleSuccess, errorCallback, {
-        enableHighAccuracy: true,
-        maximumAge: 5000 // 5 sec.
-      });
-    } catch (err) {
-      errorCallback();
-    }
-  } else {
-    errorCallback();
-  }
-}
-
-function init() {
-  watchLocation(function(coords) {
-    //document.getElementById('test').innerHTML = 'coords: ' + coords.latitude + ',' + coords.longitude;
-	alert("ppp");
-	latitud = coords.latitude;
-	longitud = coords.longitude;
-	creacionMapa();
-  }, function() {
-    alert('error');
-  });
-}
 				
