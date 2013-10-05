67497a85be355810eed0b18be2af4a31066ff868
ingchrist2003 <ingchrist2003@gmail.com>
2013-10-05 18:13:49 -0500

    actu iscroll lite

--- js/index.js
+++ js/index.js
@@ -171,6 +171,7 @@
 					document.getElementById("thelist").innerHTML=stringvar2;
 					pullDownAction();
 					myScroll.refresh();
+					document.getElementById("pullDown").innerHTML='<span class="pullDownIcon"></span><span class="pullDownLabel">Desliza para actualizar...</span>';
 				}
 				//
 				
